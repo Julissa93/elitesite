fc7afb1183ae63da9c5a8b217973842b188ddd14
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2017-11-15 17:31:02 -0500

    added claudia's links

---
+++ blog.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.6.1/fullcalendar.min.css">
+  <link rel = "stylesheet" media="print" href = "https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.6.1/fullcalendar.print.css">
+  <link href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.3/jquery-ui.css' rel='stylesheet' />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
+<body>
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class = "row">
+        <div id = "elite" class = "col-sm-6 col-md-6 col-lg-6 col-xl-6">
+          <a href="index.html" class="nav navbar-left" id="navbarBrand" style = "font-size: 23px; ">
+            <img src="images/elitelogo.png" height = "60px" width = "55px" id="navbarLogo">ELiTe
+          </a>
+        </div>
+        <div class = "col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.html#p1">Home</a></li>
+            <li><a href = "index.html#p2">About</a></li>
+            <li><a href="index.html#p3">Officers</a></li>
+            <li><a href="index.html#p4">Events</a></li>
+            <li><a href="blog.html">Blog</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class = "panel blog">
+    <div class = "container-fluid" >
+      <div class = "row">
+        <div class = "col-xs-12 col-sm-6 col-md-6 col-lg-6 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
+          <h1 class = "text-center coming-soon wow zoomIn">COMING SOON!</h1>
+          <hr class = "wow zoomIn">
+        </div>
+      </div>
+    </div>
+  </div>
+
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script>new WOW().init();</script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src = "moment.js"></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.6.1/fullcalendar.min.js"></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.1/locale/af.js"></script>
+
+
+</body>
+</html>
